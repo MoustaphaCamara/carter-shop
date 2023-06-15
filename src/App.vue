0a9969a1e2243d4bcd21de3726d8b79f423d0bc1
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-06-15 20:57:54 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <HomePage title="HomePage" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HomePage from "./pages/HomePage.vue";
+</script>
+
+<style scoped></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// import { ref } from 'vue'
+
+defineProps<{ title: string }>();
+
+// const count = ref(0)
+</script>
+
+<style scoped></style>
