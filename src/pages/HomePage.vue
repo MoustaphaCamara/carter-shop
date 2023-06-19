24bad9c0c10b5fbf883893cf2d3bb92111478185
Moustapha <cmr.mous@gmail.com>
2023-06-19 16:43:45 +0200

    tailwind init & config

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
+    <h1 className="text-3xl text-blue-600 font-bold underline">
+      {{ title }}
+    </h1>
   </div>
 </template>
 
 <script setup lang="ts">
-// import { ref } from 'vue'
-
 defineProps<{ title: string }>();
-
-// const count = ref(0)
 </script>
 
 <style scoped></style>
