4aed9a85936cbd87f4e3dcd61fcdadbd3192f4c1
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-06-20 23:07:12 +0200

    product style

--- src/components/Product/ProductList.vue
+++ src/components/Product/ProductList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div class="max-w-3xl grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
       <ProductItem :products="products" />
     </div>
   </div>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <h1 className="text-3xl text-blue-600 font-bold">HomePage</h1>
+    <div
+      class="max-w-2xl mx-auto mt-6 flex justify-center items-center flex-col"
+    >
+      <img
+        src="https://logo-marque.com/wp-content/uploads/2021/08/Akatsuki-Logo.png"
+        alt="logo"
+        class="w-32"
+      />
+      <h1
+        class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"
+      >
+        <span
+          class="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600"
+          >Manga List</span
+        >
+      </h1>
+    </div>
     <ProductList :products="products" />
   </div>
 </template>
